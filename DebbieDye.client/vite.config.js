42cd18a398c62485aba3a4a15d3d772b3a78186f
Dylan Cade <dacade.code@gmail.com>
2022-09-16 15:27:31 -0600

    dploy init

--- DebbieDye.client/vite.config.js
+++ DebbieDye.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: '../DebbieDye/deploy',
+    outDir: '../DebbieDye/client',
     sourcemap: false
   },
   server: {
